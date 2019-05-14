e0b71b20e2c1f30d8e6444c3375021e710b8b072
Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>
2019-05-15 01:23:00 +0300

    arm: include: asm: unified: mask .syntax unified for clang
    It allows to build samples/bpf for arm on target board.
    
    Signed-off-by: Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>

--- arch/arm/include/asm/swab.h
+++ arch/arm/include/asm/swab.h
@@ -25,8 +25,11 @@ static inline __attribute_const__ __u32 __arch_swahb32(__u32 x)
 	__asm__ ("rev16 %0, %1" : "=r" (x) : "r" (x));
 	return x;
 }
+
+#ifndef CONFIG_BUILD_CLANG_BPF
 #define __arch_swahb32 __arch_swahb32
 #define __arch_swab16(x) ((__u16)__arch_swahb32(x))
+#endif
 
 static inline __attribute_const__ __u32 __arch_swab32(__u32 x)
 {
